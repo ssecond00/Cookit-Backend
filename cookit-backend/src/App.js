820407fbf9aef3e0caf83da76424b3aaf60e8062
Segundo Mendez <mendezsegun@gmail.com>
2022-05-29 18:23:51 -0300

    Se modifican dependencias en package json

--- cookit-backend/src/App.js
+++ cookit-backend/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+//Express
+var express = require('express');
+//instancio el servidor
+var app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+//var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bluebird = require('bluebird');
+
+module.exports = app;
