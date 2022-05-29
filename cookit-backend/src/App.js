b882322a23877e2f03583f911c1735c01c5d8649
Segundo Mendez <mendezsegun@gmail.com>
2022-05-29 19:47:26 -0300

    push nazi

--- cookit-backend/src/App.js
+++ cookit-backend/src/App.js
@@ -1,12 +1,60 @@
 //Express
 var express = require('express');
+
 //instancio el servidor
 var app = express();
 
 
-//var path = require('path');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bluebird = require('bluebird');
+var fs = require('fs');
+
+//incorporo cors
+var cors = require('cors');
+
+//importo router
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api'); //Custom
+var utilRouter = require('./routes/utils');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+//aplico cors
+app.use(cors());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Indico las rutas de los endpoint
+app.use('/api', apiRouter);
+app.use('/', indexRouter);
+app.use('/utils/', utilRouter);
+
+//onsole.log("processENV",process.env);
+if (process.env.NODE_ENV === 'Development') {
+    require('./config').config();
+}
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+// Setup server port
+var port = process.env.PORT || 8080;
+// Escuchar en el puerto
+app.listen(port, () => {
+    console.log('Servidor de ABM Users iniciado en el puerto ', port);
+});
 
 module.exports = app;
--- cookit-backend/src/routes/api/user.route.js
+++ cookit-backend/src/routes/api/user.route.js
@@ -1,16 +1,18 @@
 var express = require('express')
 var router = express.Router()
+/*
 var UserController = require('../../controllers/users.controller');
 var UploadController = require('../../controllers/upload.controller');
 var MailController = require('../../controllers/mail.controller');
 var Authorization = require('../../auth/authorization');
-
+*/
 
 // Authorize each API with middleware and map to the Controller Functions
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
     res.send('Llegaste a la ruta de  api/user.routes');
   });
+/*
 router.post('/registration', UserController.createUser)
 router.post('/login/', UserController.loginUser)
 router.get('/',Authorization, UserController.getUsers)
@@ -21,7 +23,7 @@ router.post('/guardarImgUser',UserController.guardarImagenUser)
 router.post('/uploadImg',UploadController.uploadFilesImgUser);
 router.post('/imgUserByMail',Authorization,UserController.getImagenUserByMail)
 router.post('/sendMail',MailController.sendEmail)
-
+*/
 
 
 // Export the Router
